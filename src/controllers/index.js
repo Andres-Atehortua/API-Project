45f9273c3c5b3e9a2637a2e288e790f739ade5d2
Andres Atehortua <anderson17colombia@hotmail.com>
2020-04-23 20:00:41 +0200

    [ADD] Controllers added

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,7 @@
 module.exports = {
   HomeController: require("./home.controller"),
+  UserController: require("./user.controller"),
+  AuthController: require("./auth.controller"),
+  CommentController: require("./comment.controller"),
+  IdeaController: require("./idea.controller"),
 };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,28 @@
+let _userService = null;
+class UserController {
+  constructor(UserService) {
+    _userService = UserService;
+  }
+  async get(req, res) {
+    const { userID } = req.params;
+    const user = await _userService.get(userID);
+    return res.send(user);
+  }
+  async getAll(req, res) {
+    const users = await _userService.getAll();
+    res.send(users);
+  }
+  async update(req, res) {
+    const { body } = req;
+    const { userID } = res.params;
+    const updatedUser = await _userService.update(userID, body);
+    return res.send(updatedUser);
+  }
+  async delete(req, res) {
+    const { userID } = req.params;
+    const deletedUser = await _userService.delete(userID);
+    res.send(deletedUser);
+  }
+}
+
+module.exports = UserController;
